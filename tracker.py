1bed462d958f7349eb80a5e82ca07e28057e97d5
AZ19999 <zhualexander@yahoo.com>
2022-03-23 16:02:00 -0400

    work on transactions

--- tracker.py
+++ tracker.py
@@ -31,11 +31,11 @@ could be replaced with PostgreSQL or Pandas or straight python lists
 
 '''
 
-#from transactions import Transaction
+from transactions import Transaction
 from category import Category
 import sys
 
-#transactions = Transaction('tracker.db')
+transactions = Transaction('tracker.db')
 category = Category('tracker.db')
 
 
