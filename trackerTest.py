b77b869cd36e0c1b427458e98364361488bb121c
AZ19999 <zhualexander@yahoo.com>
2022-03-24 21:27:29 -0400

    adjusted category methods and wrote tests

--- trackerTest.py
+++ trackerTest.py
@@ -2,9 +2,21 @@ import pytest
 
 @pytest.mark.delete_test
 def test_delete():
+    testRow = category.select_one(5)
     category.delete(5)
-    
+    testRow2 = category.select_one(5)
+    assert testRow != testRow2
     
 @pytest.mark.summarize_year_test
 def test_summarizeYear():
-    print("unimplented")
+    year = 2005
+    con = sqlite3.connect('tracker.db')
+    cur = con.cursor()
+    cur.execute('''SELECT year, amount FROM categories
+                       WHERE year=(?) LIMIT 5;
+    ''',(year,))
+    z = cur.fetchall()
+    con.commit()
+    con.close()
+    y = category.summarizeyear(2005)
+    assert z == y
