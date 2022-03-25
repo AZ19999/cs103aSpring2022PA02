ab3c773f24249f0a91fc2eaadc9be3a1b735d114
zachAllon <48698694+zachAllon@users.noreply.github.com>
2022-03-24 21:04:03 -0400

    New abilities

--- tracker.py
+++ tracker.py
@@ -81,8 +81,22 @@ def process_choice(choice):
 
     #our code
 
+    elif choice=='5':
+        itemNum= int(input("Item#: "))
+        amount= int(input("Amount: "))
+        itemCat= input("Category: ")
+        itemDate= int(input("Date: "))
+        description=input("Description: ")
+        cat = {'item#':itemNum,'amount':amount,'category':itemCat,'date':itemDate,'description':description}
+        category.addTransaction(cat)
+        
+       
+        
 
-
+    elif choice=="11":
+        print("0. quit \n 1. show categories \n 2. add category \n 3. modify category \n 4. show transactions \n 5. add transaction")
+        print("6. delete transaction \n 7. summarize transactions by date \n 8. summarize transactions by month")
+        print("9. summarize transactions by year \n 10. summarize transactions by category \n 11. print this menu")
 
     else:
         print("choice",choice,"not yet implemented")
